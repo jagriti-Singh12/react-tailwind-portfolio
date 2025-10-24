8915dfc37a8ba2e5a6ef58bdb3899effce449ff3
Jagriti Singh <singhjagriti84@gmail.com>
2025-10-24 21:08:50 +0530

    updated contact form

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,23 +1,28 @@
 import { Mail, MapPin, Linkedin, Send } from "lucide-react";
-import { cn } from "../lib/utils";
-import { toast } from "sonner";
 import { useState } from "react";
 
 export const ContactSection = () => {
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [result, setResult] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+    formData.append("access_key", "778d56fa-c69d-4519-9b41-757d92d44ae1");
 
-    setTimeout(() => {
-      toast.success("Message sent!", {
-        description: "Thank you for your message. I'll get back to you soon.",
-      });
-      setIsSubmitting(false);
-    }, 1500);
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      setResult("Error");
+    }
   };
-
   return (
     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
@@ -81,7 +86,7 @@ export const ContactSection = () => {
           {/* Contact Form */}
           <div className="bg-card p-8 rounded-lg shadow-xs">
             <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
-            <form className="space-y-6" onSubmit={handleSubmit}>
+            <form className="space-y-6" onSubmit={onSubmit}>
               <div>
                 <label
                   htmlFor="name"
@@ -134,14 +139,11 @@ export const ContactSection = () => {
 
               <button
                 type="submit"
-                disabled={isSubmitting}
-                className={cn(
-                  "cosmic-button w-full flex items-center justify-center gap-2"
-                )}
+                className="cosmic-button w-full flex items-center justify-center gap-2"
               >
-                {isSubmitting ? "Sending..." : "Send Message"}
-                <Send size={16} />
+                Submit <Send size={16} />
               </button>
+              <span>{result}</span>
             </form>
           </div>
         </div>
